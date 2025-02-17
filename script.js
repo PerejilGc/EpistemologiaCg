fde7e2ab349d4d50803f6a9e49d90403d9594300
PerejilCg <ah949546@gmail.com>
2025-02-16 19:34:54 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper(".mySwiper", {
+    
+    slidesPerView: 1,
+    spaceBetween: 80,
+    grabCursor: true,
+    loop:true,
+    breakpoints: {
+        991: {
+            slidesPerView:3
+        }
+    }
+})
+
+
